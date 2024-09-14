b224e4938e5a3d2af0b376180e00efbd14bbc3f3
Luis David <lrambao5432@gmail.com>
2024-09-13 22:13:34 -0500

    :recycle: Improve code to button component

--- frontend-krist/src/components/Button.jsx
+++ frontend-krist/src/components/Button.jsx
@@ -3,7 +3,7 @@ import { Icon } from "lucide-react";
 const Button = ({ onClick, text, leftIcon, righIcon, type = "submit" }) => {
   return (
     <button
-      onClick={() => onClick()}
+      onClick={onClick}
       type={type}
       className="btn relative w-full px-6 py-2 border-2 border-transparent rounded bg-black text-white cursor-pointer mt-3 transition-all duration-300 ease-in-out shadow-md hover:bg-white hover:text-black hover:-translate-y-1 hover:shadow-sm hover:border-black"
     >
