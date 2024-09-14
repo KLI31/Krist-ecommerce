ee358d1d69873d233585ec3408ed2fa4b463d9fa
Luis David <lrambao5432@gmail.com>
2024-09-14 18:42:20 -0500

    :art: Improve input component

--- frontend-krist/src/components/Input.jsx
+++ frontend-krist/src/components/Input.jsx
@@ -1,19 +1,32 @@
-// Supports weights 100-900
+import { forwardRef } from "react";
 import "@fontsource-variable/jost";
 
-const Input = ({ placeholder, value, name, nameLabel, onChange }) => {
-  return (
-    <label htmlFor={name} className="font-normal flex flex-col">
-      {nameLabel && <span>{nameLabel}:</span>}
-      <input
-        placeholder={placeholder}
-        className="p-4 border-2 border-solid border-black rounded-md mb-4 focus:outline-none focus:ring-1 focus:ring-black"
-        name={name}
-        value={value}
-        onChange={onChange}
-      />
-    </label>
-  );
-};
+// eslint-disable-next-line react/display-name
+const Input = forwardRef(
+  (
+    { placeholder, value, name, nameLabel, onChange, errors = {}, type },
+    ref
+  ) => {
+    return (
+      <label htmlFor={name} className="font-normal flex flex-col">
+        {nameLabel && <span>{nameLabel}:</span>}
+        <input
+          type={type}
+          placeholder={placeholder}
+          className={`p-4 border-2 border-solid border-black rounded-md mb-4 focus:outline-none focus:ring-1 focus:ring-black ${
+            errors && errors[name] ? "border-red-500 mb-2" : ""
+          }`}
+          name={name}
+          value={value}
+          onChange={onChange}
+          ref={ref}
+        />
+        {errors && errors[name] && (
+          <span className="text-red-500 text-sm">{errors[name].message}</span>
+        )}
+      </label>
+    );
+  }
+);
 
 export default Input;
