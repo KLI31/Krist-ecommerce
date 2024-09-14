7f39ec2a6c5acd132fbe6869b6fb3774a6a3ba56
Luis David <lrambao5432@gmail.com>
2024-09-13 22:12:45 -0500

    :bug: Fix event bubbling in the modal component

--- frontend-krist/src/components/Modal.jsx
+++ frontend-krist/src/components/Modal.jsx
@@ -2,11 +2,12 @@ const Modal = ({ open, children, onClose }) => {
   return (
     <div
       className={`fixed inset-0 flex justify-center items-center transition-colors ${
-        open ? "bg-black bg-opacity-50" : "bg-transparent"
+        open ? "bg-black bg-opacity-55" : "invisible"
       }`}
       onClick={onClose}
     >
       <div
+        onClick={(e) => e.stopPropagation()}
         className={`bg-white p-6 transition-all ${
           open ? "scale-100 opacity-100" : "scale-125 opacity-0"
         }`}
