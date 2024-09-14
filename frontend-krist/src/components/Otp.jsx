4f88d0a9324f1e1ea1bc2178e96837442c5a0f61
Luis David <lrambao5432@gmail.com>
2024-09-13 22:11:41 -0500

    :recycle: Refactor otp component and otp page

--- frontend-krist/src/components/Otp.jsx
+++ frontend-krist/src/components/Otp.jsx
@@ -1,12 +1,7 @@
-import { useState, useRef, useEffect } from "react";
+import { useRef, useEffect } from "react";
 import Button from "@/components/Button";
-import Modal from "@/components/Modal";
 
-export default function OTPInput() {
-  const [otp, setOtp] = useState(Array(5).fill(""));
-  const [open, setOpen] = useState(false);
-  const [isValid, setIsValid] = useState(null);
-  const [showMessage, setShowMessage] = useState(false);
+export default function OTPInput({ otp, setOtp, onClick }) {
   const inputRefs = useRef([]);
 
   useEffect(() => {
@@ -33,32 +28,6 @@ export default function OTPInput() {
     }
   };
 
-  const verifyOTP = async () => {
-    const isValidOTP = otp.join("") === "12345";
-    setIsValid(isValidOTP);
-    setOpen(true);
-    setShowMessage(true);
-
-    // Aquí es donde normalmente harías una llamada a tu API
-    // const response = await fetch('/api/verify-otp', {
-    //   method: 'POST',
-    //   headers: { 'Content-Type': 'application/json' },
-    //   body: JSON.stringify({ otp: otp.join('') })
-    // })
-    // const data = await response.json()
-    // setIsValid(data.valid)
-  };
-
-  useEffect(() => {
-    if (isValid !== null) {
-      setShowMessage(true);
-      const timer = setTimeout(() => {
-        setShowMessage(false);
-      }, 4000);
-      return () => clearTimeout(timer);
-    }
-  }, [isValid]);
-
   return (
     <div style={styles.container}>
       <div style={styles.inputGroup}>
@@ -76,26 +45,7 @@ export default function OTPInput() {
           />
         ))}
       </div>
-      <Button onClick={() => verifyOTP()} text="Verificar" />
-      {/* <button onClick={() => setOpen(true)}>Modal</button> */}
-      {open && (
-        <Modal open={open} onClose={() => setOpen(false)}>
-          <div>
-            <h2>{isValid ? "¡Éxito!" : "Error"}</h2>
-            <button onClick={() => setOpen(false)}>Cerrar</button>
-          </div>
-        </Modal>
-      )}
-      {showMessage && isValid !== null && (
-        <p
-          style={{
-            ...styles.message,
-            ...(isValid ? styles.valid : styles.invalid),
-          }}
-        >
-          {isValid ? "OTP válido!" : "OTP inválido. Intenta de nuevo."}
-        </p>
-      )}
+      <Button onClick={onClick} text="Verificar" />
     </div>
   );
 }
--- frontend-krist/src/pages/OtpPage.jsx
+++ frontend-krist/src/pages/OtpPage.jsx
@@ -3,8 +3,50 @@ import imageOtp from "@/assets/image-otp.webp";
 import OtpInput from "@/components/Otp";
 import { Link } from "react-router-dom";
 import { ChevronLeft } from "lucide-react";
+import { useEffect, useState } from "react";
+import Modal from "@/components/Modal";
+import Button from "@/components/Button";
+import { Check, X as ErrorIcon } from "lucide-react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const OtpPage = () => {
+  const [otp, setOtp] = useState(Array(5).fill(""));
+  const [isValid, setIsValid] = useState(null);
+  const [open, setOpen] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const { email } = location.state || { email: "example123@gmail.com" };
+
+  const verifyOTP = async () => {
+    const isValidOTP = otp.join("") === "12345";
+    setIsValid(isValidOTP);
+    setOpen(true);
+
+    // Aquí es donde normalmente harías una llamada a tu API
+    // const response = await fetch('/api/verify-otp', {
+    //   method: 'POST',
+    //   headers: { 'Content-Type': 'application/json' },
+    //   body: JSON.stringify({ otp: otp.join('') })
+    // })
+    // const data = await response.json()
+    // setIsValid(data.valid)
+  };
+
+  useEffect(() => {
+    if (isValid) {
+      const timer = setTimeout(() => {
+        navigate("/");
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [isValid, navigate]);
+
+  const handleModalClose = (e) => {
+    e.stopPropagation();
+    setOpen(false);
+  };
+
   return (
     <DividerScreen>
       <div className="flex-[2] flex justify-center items-center">
@@ -28,12 +70,40 @@ const OtpPage = () => {
           </Link>
           <h1 className="font-bold text-[30px]">Codigo Otp</h1>
           <p className="mb-3 text-[#666] text-wrap">
-            Te hemos enviado un código de verificación a tu correo electrónico
-            example12@gmail.com
+            Te hemos enviado un código de verificación a tu correo electrónico:{" "}
+            {email}
           </p>
           <div className="mt-4">
-            <OtpInput />
+            <OtpInput otp={otp} setOtp={setOtp} onClick={() => verifyOTP()} />
           </div>
+          <Modal open={open} onClose={() => handleModalClose}>
+            <div className="flex flex-col items-center justify-center animate-fade-in animate-duration-300">
+              <div className="relative flex items-center justify-center w-36 h-36 bg-gray-300 rounded-full mb-4 animate-scale-in animate-duration-300">
+                <div className="absolute w-28 h-28 bg-[#131118] bg-opacity-20 rounded-full flex items-center justify-center animate-scale-in animate-delay-100">
+                  <div className="absolute w-20 h-20 bg-[#131118] rounded-full flex items-center justify-center animate-scale-in animate-delay-200">
+                    <div className="w-8 h-8 bg-white rounded-full flex items-center justify-center animate-scale-in animate-delay-300">
+                      {isValid ? (
+                        <Check size={24} strokeWidth={3} />
+                      ) : (
+                        <ErrorIcon size={24} strokeWidth={3} />
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <h2 className="text-xl font-bold">
+                {isValid
+                  ? "Contraseña cambiada correctamente"
+                  : "Intenta de nuevo"}
+              </h2>
+              <p className="break-words">
+                {isValid
+                  ? "Tu contraseña ha sido cambiada exitosamente."
+                  : "El codigo ingresado es incorrecto. Por favor, intenta de nuevo."}
+              </p>
+              <Button onClick={() => setOpen(false)} text="Cerrar" />
+            </div>
+          </Modal>
         </div>
       </div>
     </DividerScreen>
