3d5d4fdc3013e8fd34dff520c6c9830a4cf3724b
Luis David <lrambao5432@gmail.com>
2024-09-15 22:21:14 -0500

    :recycle: Create schemas otp and user

---
+++ backend/models/User.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    profilePic: {
+      type: String,
+      default:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
