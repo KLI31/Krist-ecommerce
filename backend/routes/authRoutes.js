0ed1a558ac3dbbbf81b7564253db00037d28cec2
Luis David <lrambao5432@gmail.com>
2024-09-22 13:49:25 -0500

    :recycle: Use middlewares

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,14 +3,18 @@ import {
   login,
   sendOtp,
   resetPassword,
+  verifyOtp,
 } from "../controllers/auth.js";
 import express from "express";
+import { checkEmail } from "../middleware/checkEmail.js";
+import { checkOtpVerified } from "../middleware/checkOtpVerified.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/sendotp", sendOtp);
-router.post("/resetpassword", resetPassword);
+router.post("/verifyotp", verifyOtp, checkEmail, checkOtpVerified);
+router.post("/resetpassword", resetPassword, checkOtpVerified, resetPassword);
 
 export default router;
